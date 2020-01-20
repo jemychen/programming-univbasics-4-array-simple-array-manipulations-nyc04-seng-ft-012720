784a265d9334779cbff3ccc410ac581a1301e2b4
Jemy Chen <jemychen@live.com>
2020-01-20 20:12:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,49 @@
+def using_push(array, string)
+  #colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  array.push("violet")
+end
+
+
+def using_unshift(array, string)
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+  ice_cream_brands.shift(2)
+end
+
+def using_concat(array)
+  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+  more_favs = ["sports cars", "flatiron school"]
+  my_favorite_things.concat("more_favs")
+end
+
+#def using_insert()
+#  Array.insert[4]
+#end
+
+#def using_uniq()
+#  Array.uniq
+#def
+
+#def using_flatten(Array)
+#end
+
+#def using_delete()
+#end
+
+#def using_delete_at()
+#end
